e6ac64021e7024257e5e10c8393ba038552dcc86
temashik <temashik21@gmail.com>
2022-06-07 15:08:00 +0300

    Removed ignored files

--- dist/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TYPES = void 0;
-exports.TYPES = {
-    Application: Symbol.for('Application'),
-    ILogger: Symbol.for('ILogger'),
-    DatabaseController: Symbol.for('DatabaseController'),
-};
-//# sourceMappingURL=types.js.map
